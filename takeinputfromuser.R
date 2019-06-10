665ee2ccde2d3b252645e626995ccdb44b8ad373
Le <le.chang@mail.mcgill.ca>
2019-06-10 13:38:16 -0400

    finished feature

--- takeinputfromuser.R
+++ takeinputfromuser.R
@@ -1,5 +1,6 @@
 my.name <- readline(prompt="Enter name: ")
 my.age <- readline(prompt="Enter age: ")
+my.greeting <- readline(prompt="How are you?")
 # convert character into integer
 my.age <- as.integer(my.age)
-print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old."))
+print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old.", "I am ", my.greeting))
